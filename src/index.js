fce9ac9f5923190d0c320aef9435821a8ba8745f
cheesecake2020 <well277balanced@gmail.com>
2021-09-16 14:48:12 +0900

    implement simple html

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function compornent() {
+    const div = document.createElement('div');
+    const array = ['Hello', 'webpack']
+    div.innerHTML= _.join(array,' ')
+    return div;
+}
+document.body.appendChild(compornent());
